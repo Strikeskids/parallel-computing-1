a8e980d3985deb29a8a90056945ae8cb598d2f50
cdeboor <2015cdeboor@tjhsst.edu>
2014-12-19 13:03:44 -0500

    Added render to file code

--- fractal/mandelbrot.c
+++ fractal/mandelbrot.c
@@ -29,68 +29,43 @@ double rect[] = {
 	4.0, 3.0
 };
 
-int data[M][N];
-
 int iterations = 40;
 
 GLuint cornersId;
+
 GLuint mandelbrotProgramId;
+GLuint iterationsUniformId;
 
 void drawMandlebrot() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glUseProgram(mandlebrotProgramId);
 
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, iterationsTexture);
-	int textureActive = 0;
-	glUniform1iv(iterationsTextureUniform, 1, &textureActive);
-	glUniform1iv(iterationsUniform, 1, &iterations);
+	glUniform1iv(iterationsUniformId, 1, &iterations);
 
-	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cornersId);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
 	glDrawArrays(GL_QUADS, 0, 4);
 
 	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glUseProgram(0);
 }
 
-GLuint positionBuffer;
-GLuint mandlebrotProgram;
-GLuint iterationsUniform;
-GLuint iterationsTexture;
-GLuint iterationsTextureUniform;
-
 void displayfunc(void) {
-
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	drawMandlebrot();
 	glutSwapBuffers();
 }
 
 void initializePrograms() {
 	char *mandleShaders[] = {"mandle.vert", "mandle.frag", NULL};
 
-	mandlebrotProgram = initializeProgram(mandleShaders);
-	iterationsTextureUniform = glGetUniformLocation(mandlebrotProgram, "iterationsTexture");
-	iterationsUniform = glGetUniformLocation(mandlebrotProgram, "iterations");
-}
-
-void reloadIterationTexture() {
-	glBindTexture(GL_TEXTURE_2D, iterationsTexture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, N, M, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void createIterationTexture() {
-	glGenTextures(1, &iterationsTexture);
-
-	glBindTexture(GL_TEXTURE_2D, iterationsTexture);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glBindTexture(GL_TEXTURE_2D, 0);
-
-	reloadIterationTexture();
+	mandlebrotProgramId = initializeProgram(mandleShaders);
+	
+	iterationsUniformId = glGetUniformLocation(mandlebrotProgram, "iterations");
 }
 
 void initialize(int argc, char **argv) {
@@ -109,36 +84,15 @@ void initialize(int argc, char **argv) {
 	glBindVertexArray(vao);
 }
 
-void initializeDrawRect() {
-	glGenBuffers(1, &positionBuffer);
+void initializeCorners() {
+	glGenBuffers(1, &cornersId);
 
-	glBindBuffer(GL_ARRAY_BUFFER, positionBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, cornersId);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(RECT_CORNERS), RECT_CORNERS, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
-void computeMandlebrot() {
-	int i, j;
-	for (i=0;i<N;++i) {
-		for (j=0;j<M;++j) {
-			double cx = rect[0] + (i * 1.0 / N - 0.5) * rect[2];
-			double cy = rect[1] + (j * 1.0 / M - 0.5) * rect[3];
-			double zx = 0, zy = 0;
-			int it;
-			for (it=0;it<iterations&&(zx*zx+zy*zy)<4;++it) {
-				double ox = zx;
-				double oy = zy;
-				zx = ox * ox - oy * oy + cx;
-				zy = 2 * ox * oy + cy;
-			}
-			data[j][i] = it;
-		}
-	}
-}
-
 void recalculate() {
-	computeMandlebrot();
-	reloadIterationTexture();
 	glutPostRedisplay();
 }
 
@@ -181,18 +135,29 @@ void mouse(int button, int state, int x, int y) {
 	}
 }
 
+void render() {
+	RenderTarget rt;
+	initRenderTarget(&rt, N, M);
+	prerenderToTarget(&rt);
+	drawMandlebrot();
+	postrenderToTarget(&rt);
+	FILE *output = fopen("frame.ppm", "w");
+	saveTextureToFile(output, &rt);
+	fclose(output);
+}
+
 int main(int argc, char **argv) {
 	initialize(argc, argv);
 	initializePrograms();
-	initializeDrawRect();
-	computeMandlebrot();
-	createIterationTexture();
+	initializeCorners();
 
 	printf("%s\n", glGetString(GL_VERSION));
 
 	glutDisplayFunc(displayfunc);
 	glutKeyboardFunc(keyfunc);
 	glutMouseFunc(mouse);
+
+	render();
 
 	glutMainLoop();
 
